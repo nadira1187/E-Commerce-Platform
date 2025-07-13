966775b85cc992d62b5d62b870f22e59b688ad4e
nadira1187 <nadira35-1187@s.diu.edu.bd>
2025-07-14 00:10:33 +0600

    added more css styles in home page with floating icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ export default {
     './resources/**/*.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          700: '#1D4ED8',
+          800: '#1E40AF',
+          300: '#93C5FD',
+          900: '#1E3A8A'
+        }
+      }
+    },
   },
   plugins: [],
 };
